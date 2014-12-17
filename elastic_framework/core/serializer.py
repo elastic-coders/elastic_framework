6a5da99d0afee0724e97c7990341da58491cbb7b
davidescatto <davidescatto@gmail.com>
2014-12-17 07:59:03 -0800

    add serializer field management

---
+++ elastic_framework/core/serializer.py
@@ -0,0 +1,36 @@
+''' extension of DRF serializer
+
+'''
+
+from rest_framework.fields import ChoiceField
+
+
+class DisplayFieldWithChoice(ChoiceField):
+    ''' Used into serializer to display choiced field
+    This field has a model mapping so you can define it as writable too
+
+    '''
+
+    def __init__(self, choices=(), *args, **kwargs):
+        super(DisplayFieldWithChoice, self).\
+            __init__(choices=choices, *args, **kwargs)
+        # prepare dictionary with direct and reverted choices
+        self.revert = {
+            value: key for key, value in self.choices.items()
+        }
+        self.forward = {
+            key: value for key, value in self.choices.items()
+        }
+
+    def valid_value(self, value):
+        return value in self.forward
+
+    def from_native(self, value):
+        try:
+            return self.revert[value]
+        except KeyError:
+            raise ValidationError('{}: invalid value for choice field'.\
+                                      format(value))
+
+    def to_native(self, value):
+        return self.forward[value]
